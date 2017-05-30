de4e013105622acda393c4f0a64e672076103071
SomeoneKnowNothing <971070045@qq.com>
2017-05-30 18:59:17 +0800

    initial commit

---
+++ app/src/main/java/com/sxq/backgammon/model/Point.java
@@ -0,0 +1,27 @@
+package com.sxq.backgammon.model;
+
+/**
+ * Created by SXQ on 2017/5/28.
+ */
+
+public class Point {
+
+    private int mX ;
+    private int mY ;
+
+    public int getmX() {
+        return mX;
+    }
+
+    public void setmX(int mX) {
+        this.mX = mX;
+    }
+
+    public int getmY() {
+        return mY;
+    }
+
+    public void setmY(int mY) {
+        this.mY = mY;
+    }
+}
